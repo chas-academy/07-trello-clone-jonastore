a7eb39307a0b7ebe087cf6613a90252acd5a7f9e
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-01-24 12:59:10 +0100

    added a color picker to change card color

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
 	$( function() {
-	    $( "#draggable" ).draggable({ snap: ".ui-widget-header" }).resizable();;
+	    $( ".draggable" ).draggable({ snap: ".ui-widget-header" }).resizable();;
 	    $( "#droppable"  ).droppable({
-	      accept: "#draggable",
+	      accept: ".draggable",
       		classes: {
         	"ui-droppable-active": "ui-state-active",
         	"ui-droppable-hover": "ui-state-hover"
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
 	    });
 	     $( "#droppable2"  ).droppable({
-	     	accept: "#draggable",
+	     	accept: ".draggable",
       		classes: {
         	"ui-droppable-active": "ui-state-active",
         	"ui-droppable-hover": "ui-state-hover"
@@ -30,7 +30,7 @@ $(document).ready(function() {
 	      }
 	    });
 	     $( "#droppable3"  ).droppable({
-	     	accept: "#draggable",
+	     	accept: ".draggable",
       		classes: {
         	"ui-droppable-active": "ui-state-active",
         	"ui-droppable-hover": "ui-state-hover"
@@ -61,21 +61,33 @@ $(document).ready(function() {
     $( "#tabs" ).tabs();
   	} );
 
+  	$('#colorpicker').on('change', function (e) {
 
+	var hexColor = '#' +this.value;
+
+	$(".draggable").css("background", hexColor);
 
 });
 
 
 
+});
+
+var clone = '<div class="draggable" class="ui-widget-content"><button class="delete">X</button><div id="tabs"><ul><li><a href="#tabs-2">Card color</a></li><li><a href="#tabs-1">Set deadline</a></li></ul><p>Drag me around</p><button id="infobutton">More info</button><div id="dialog" title="Basic dialog"><p>This is the information that will appear on the card.</p></div><div id="tabs-1"><input type="text" name="date" id="date" placeholder="Set deadline"></div><div id="tabs-2">something here</div></div></div>'
+
 $(function(){
     $('#createbutton').on('click', function(){
-		var clone = $('#draggable').clone();
+		var clone = $('.draggable').clone();
         $("#wrapper").append(clone);
     });
 });
 
 $("body").on("click", ".delete", function(event) {
-    $(event.target).closest("#draggable").remove();
+    $(event.target).closest(".draggable").remove();
   });
+
+
+
+
 
 
